de1c68b598e47d6be5b20644ba85da34869de93e
Naimas1 <114872107+Naimas1@users.noreply.github.com>
2023-06-29 09:09:19 +0300

    Program.cs

--- Program.cs
+++ Program.cs
@@ -1,101 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Program;
-class Money
+namespace Program
 {
-    private int dollars;
-    private int cents;
-
-
-    public Money(int dollars, int cents)
-
+    internal class Program
     {
-        this.dollars = dollars;
-        this.cents = cents;
-    }
-
-    public int Cents { get; internal set; }
-    public object Dollars { get; internal set; }
-
-    public void DisplayAmount()
-    {
-        Console.WriteLine($"Amount: {dollars}.{cents:00}");
-    }
-
-    public void SetAmount(int dollars, int cents)
-    {
-        this.dollars = dollars;
-        this.cents = cents;
-    }
-}
-
-class Product
-{
-    private string name;
-    private Money price;
-
-    public Product(string name, Money price)
-    {
-        this.name = name;
-        this.price = price;
-    }
-
-    public void DisplayPrice()
-    {
-        Console.WriteLine($"Price of {name}: ");
-        price.DisplayAmount();
-    }
-
-    public void DecreasePrice(int dollars, int cents)
-    {
-        int totalCents = priceCents(price) - dollars * 100 - cents;
-        if (totalCents < 0)
+        static void Main(string[] args)
         {
-            Console.WriteLine("New price cannot be negative.");
-            return;
+            Console.WriteLine("Hello, World!");
         }
-
-        int newDollars = totalCents / 100;
-        int newCents = totalCents % 100;
-        price.SetAmount(newDollars, newCents);
-    }
-
-    private int priceCents(Money money)
-    {
-        return moneyCents(money) + dollarsToCents(money.Dollars);
-    }
-
-    private int dollarsToCents(object dollars)
-    {
-        throw new NotImplementedException();
-    }
-
-    private int moneyCents(Money money)
-    {
-        return money.Cents;
-    }
-
-    private int dollarsToCents(int dollars)
-    {
-        return dollars * 100;
-    }
-}
-
-internal class Proga
-{
-    static void Main(string[] args)
-    {
-        Money money = new Money(10, 50);
-        money.DisplayAmount();
-
-        Product product = new Product("Example Product", money);
-        product.DisplayPrice();
-
-        product.DecreasePrice(2, 30);
-        product.DisplayPrice();
     }
 }
